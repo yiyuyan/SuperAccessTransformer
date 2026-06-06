834729c8ddae0a11245c5accfc1785410a331938
yiyuyan <179544143@qq.com>
2026-06-06 19:28:28 +0800

    update test codes for arch neo1.21

--- src/main/java/cn/ksmcbrigade/testsat/neoforge/TestsatNeoForge.java
+++ src/main/java/cn/ksmcbrigade/testsat/neoforge/TestsatNeoForge.java
@@ -1,9 +1,11 @@
 package cn.ksmcbrigade.testsat.neoforge;
 
 import cn.ksmcbrigade.testsat.Testsat;
+import net.neoforged.neoforge.common.util.Lazy;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.network.registration.NetworkRegistry;
 
 @Mod(Testsat.MOD_ID)
 public final class TestsatNeoForge {
@@ -11,6 +13,10 @@ public final class TestsatNeoForge {
         // Run our common setup.
         Minecraft.getInstance().timer = new DeltaTracker.Timer(200F,0L,(x)->Minecraft.getInstance().getTickTargetMillis(x)/1.2F);
         System.out.println("nOW TIMER: "+Minecraft.getInstance().timer.msPerTick);
+        Lazy<String> l = new Lazy<>(() -> "hi");
+
+        System.out.println("lazy: "+l.get());
+        System.out.println("network: "+ NetworkRegistry.ATTRIBUTE_FLOW.name());
         Testsat.init();
     }
 }
